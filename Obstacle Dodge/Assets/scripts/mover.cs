7f24976fab8cf40b6a6569a593fcd5e0d011859a
shiveshramdhan <9028408@student.zadkine.nl>
2026-03-03 11:58:51 +0100

    .

---
+++ Obstacle Dodge/Assets/scripts/mover.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class mover : MonoBehaviour
+{
+
+  float xValue = 0f;
+  float yValue = 0.01f;
+  float zValue = 0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+         }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
