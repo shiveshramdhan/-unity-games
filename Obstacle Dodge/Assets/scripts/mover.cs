7d7e70a4ed19a8638b74aab164f881875b0020fa
shiveshramdhan <9028408@student.zadkine.nl>
2026-03-10 11:54:16 +0100

    .

--- Obstacle Dodge/Assets/scripts/mover.cs
+++ Obstacle Dodge/Assets/scripts/mover.cs
@@ -3,20 +3,20 @@ using UnityEngine;
 public class mover : MonoBehaviour
 {
 
-   [SerializeField] float xValue = 0f;
- [SerializeField] float yValue = 0.01f;
-  [SerializeField] float zValue = 0f;
-
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+ void Start() {
         
-         }
+    }
 
     // Update is called once per frame
     void Update()
     {
+      
+    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+    float yValue = 0f;
+    float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
     }
 }
