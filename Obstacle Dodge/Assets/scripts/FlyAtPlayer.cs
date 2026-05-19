2d5e78a38bbce4e6b333959a24d93851b6aad528
shiveshramdhan <9028408@student.zadkine.nl>
2026-05-19 10:54:44 +0200

    .

--- Obstacle Dodge/Assets/scripts/FlyAtPlayer.cs
+++ Obstacle Dodge/Assets/scripts/FlyAtPlayer.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class FlyAtPlayer : MonoBehaviour
 {
-    [SerializeField] Transform player;
-    Vector3 Playerposition;
+  
+    
     void Start()
     {
         
@@ -12,6 +12,6 @@ public class FlyAtPlayer : MonoBehaviour
     
     void Update()
     {
-        Transform.Position = Vector3.MoveTowards(Transform.Position, Playerposition, 1f);
+        
     }
 }
