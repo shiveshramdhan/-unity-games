62dc58b4e35b44ee20b864727a6f97711abc4fa4
shiveshramdhan <9028408@student.zadkine.nl>
2026-05-19 12:01:05 +0200

    .

--- Obstacle dodge 2/Assets/scripts/mover.cs
+++ Obstacle dodge 2/Assets/scripts/mover.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-   [SerializeField] float xvalue = 0f;
-   [SerializeField] float yvalue = 0.01f;
-    [SerializeField] float zvalue = 0f;
+   [SerializeField] float moveSpeed = 10f;
     void Start()
     {
         
@@ -13,6 +11,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
     
     void Update()
     {
+        float xvalue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yvalue = 0f;
+        float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xvalue,yvalue,zvalue);
     }
 }
