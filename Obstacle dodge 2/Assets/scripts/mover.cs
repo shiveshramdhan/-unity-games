c53c3c62646433ba24f53b5ddbe1acbe5d42b51a
shiveshramdhan <9028408@student.zadkine.nl>
2026-06-04 22:47:24 +0200

    .

--- Obstacle dodge 2/Assets/scripts/mover.cs
+++ Obstacle dodge 2/Assets/scripts/mover.cs
@@ -5,15 +5,28 @@ public class NewMonoBehaviourScript : MonoBehaviour
    [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use the arrow keys to move the player around.");
+        Debug.Log("Avoid obstacles and reach the goal to win!");
+    }
+
+    void MovePlayer()
     {
         float xvalue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yvalue = 0f;
         float zvalue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xvalue,yvalue,zvalue);
     }
+
 }
